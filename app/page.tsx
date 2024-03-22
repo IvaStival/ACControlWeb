bef9d85b6b836b4e3b35c6457df14254032dcba0
iva stival <ivastival@ivas-MacBook-Air.local>
2024-03-22 15:11:18 -0300

    feat: Add new title on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Chart from "./components/Chart";
+import Title from "./components/Tittle/Title";
 
 export default function Home() {
   return (
     <div className="main-content">
+      <Title title="ACControl" />
       <Chart />
     </div>
   );
