6870d34548bb366f05b3dbe926f043868a37e976
iva stival <ivastival@ivas-MacBook-Air.local>
2024-03-22 15:36:54 -0300

    fix: add font bold again

--- app/components/Tittle/Title.tsx
+++ app/components/Tittle/Title.tsx
@@ -1,6 +1,6 @@
 const Title = ({ title }: { title: string }) => {
   return (
-    <h1 className="absolute left-0 top-0 mt-10 ml-10 text-xl font-medium">
+    <h1 className="absolute left-0 top-0 mt-10 ml-10 text-xl font-bold">
       {title}
     </h1>
   );
