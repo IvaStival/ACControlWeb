d76aafdf2506366bbba776ead1083a26e16d0234
iva stival <ivastival@ivas-MacBook-Air.local>
2024-03-22 15:15:43 -0300

    feat: Add a new font black for title

--- app/components/Tittle/Title.tsx
+++ app/components/Tittle/Title.tsx
@@ -1,5 +1,9 @@
 const Title = ({ title }: { title: string }) => {
-  return <h1 className="absolute left-0 top-0 mt-10 ml-10 text-xl">{title}</h1>;
+  return (
+    <h1 className="absolute left-0 top-0 mt-10 ml-10 text-xl font-black">
+      {title}
+    </h1>
+  );
 };
 
 export default Title;
